e63e256c6af51bb7a527876f157a9b171a154f01
刘吉祥 <1423806246@qq.com>
2022-11-15 19:32:36 +0800

    'js完善'

--- js/SecondHandList.js
+++ js/SecondHandList.js
@@ -10,10 +10,14 @@ var top_span = document.getElementsByClassName('top-span')
 // 获取区域显示隐藏
 var top1 = document.getElementsByClassName('top1')
 
+// 找到阻止默认事件
+var fnag_ = document.getElementsByClassName('fnag')[0]
 
 for (var i = 0; i < top_.length; i++) {
     top_[i].setAttribute('index', i);
     top_[i].onclick = function() {
+        model.style.position = "absolute";
+        model.style.top = '0';
         var index = this.getAttribute('index')
         for (var k = 0; k < top_span.length; k++) {
             top_span[k].id = ''
@@ -27,27 +31,27 @@ for (var i = 0; i < top_.length; i++) {
         for (var q = 0; q < top1.length; q++) {
             top1[q].style.display = "";
             top1[index].style.display = "block";
+            top1[q].onclick = function() {
+                fnag_.onclick = function() {
+                    return false
+                }
+                top1[index].style.display = "none"
+                model.style.position = "";
+                model.style.top = '';
+                top_a[index].id = '';
+                top_span[index].id = '';
+            }
         }
-        model.style.position = "absolute";
-        model.style.top = '0';
+
 
     }
 }
 
-
-// 当点击蒙版的时候消失
-// top1.onclick = function() {
-//     this.style.display = "none";
-//     model.style.position = "";
-//     model.style.top = '';
+// for (var r = 0; r < top1.length; r++) {
+//     top1[r].onclick = function() {
+//         top1[r].style.display = "none";
+//         model.style.position = "";
+//         model.style.top = '';
+//         return false
+//     }
 // }
-
-
-for (var r = 0; r < top1.length; r++) {
-    top1[r].onclick = function() {
-        top1[r].style.display = "none";
-        model.style.position = "";
-        model.style.top = '';
-        return false
-    }
-}
